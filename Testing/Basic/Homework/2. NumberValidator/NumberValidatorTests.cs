daaa494ee51afbcad6d82d07238e884e44593268
Maxim Mineev <mms1401@ya.ru>
2024-11-02 17:08:46 +0500

    NumberValidatorTests. Декомпозиция перенагруженного теста, удаление лишних проверок, добавление недостающих

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,6 +1,5 @@
-﻿
+﻿using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
@@ -8,24 +7,139 @@ namespace HomeExercise.Tasks.NumberValidator;
 public class NumberValidatorTests
 {
     [Test]
-    public void Test()
+    public void NumberValidator_Throws_WhenNegativePrecision()
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        Action action = () => new NumberValidator(-1, 2, true);
+        action.Should().Throw<ArgumentException>();
+    }
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    [Test]
+    public void NumberValidator_Throws_WhenNegativeScale()
+    {
+        Action action = () => new NumberValidator(2, -1, true);
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void NumberValidator_Throws_WhenScaleGreaterThanPrecision()
+    {
+        Action action = () => new NumberValidator(2, 3, true);
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void NumberValidator_Throws_WhenScaleEqualPrecision()
+    {
+        Action action = () => new NumberValidator(2, 2, true);
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void NumberValidator_DontThrows_WhenPrecisionIsPositive()
+    {
+        Action action = () => new NumberValidator(2, 1, true);
+        action.Should().NotThrow<Exception>();
+    }
+
+    [Test]
+    public void NumberValidator_DontThrows_WhenScaleLessThanPrecision()
+    {
+        Action action = () => new NumberValidator(2, 1, true);
+        action.Should().NotThrow<Exception>();
+    }
+
+    [TestCase(1, true, "1")]
+    [TestCase(2, true, "21")]
+    [TestCase(3, true, "13")]
+    [TestCase(5, true, "514")]
+    [TestCase(10, false, "-314")]
+    [TestCase(2, false, "-3")]
+    [TestCase(3, false, "-99")]
+    public void NumberValidator_ReturnsTrue_WithValidIntegerNumbers_WithScaleZero(int precision, bool onlyPositive,
+        string number) => TestWithValidParameters(precision, 0, onlyPositive, number);
+
+    [TestCase(1, true, "11")]
+    [TestCase(2, true, "001")]
+    [TestCase(3, true, "4134124")]
+    [TestCase(10, false, "-031442424324243")]
+    [TestCase(2, false, "-13")]
+    [TestCase(3, false, "-993")]
+    public void NumberValidator_ReturnsFalse_WithInvalidIntegerNumbers_WithScaleZero(int precision,
+        bool onlyPositive, string number) => TestWithInvalidParameters(precision, 0, onlyPositive, number);
+
+    [TestCase(2, 1, true, "1.1")]
+    [TestCase(4, 3, true, "2.21")]
+    [TestCase(4, 3, true, "0.000")]
+    [TestCase(4, 3, true, "00.00")]
+    [TestCase(5, 1, true, "51.4")]
+    [TestCase(10, 5, false, "-31.414")]
+    [TestCase(3, 1, false, "-3.2")]
+    [TestCase(2, 1, false, "-1")]
+    public void NumberValidator_ReturnsTrue_WithValidFloatNumbers(int precision, int scale, bool onlyPositive,
+        string number) => TestWithValidParameters(precision, scale, onlyPositive, number);
+
+    [TestCase(2, 1, true, "1.12")]
+    [TestCase(4, 3, true, "2.2112")]
+    [TestCase(5, 1, true, "51.43")]
+    [TestCase(10, 5, false, "-31.41431231")]
+    [TestCase(3, 1, false, "-3.21")]
+    [TestCase(2, 1, false, "-1.1")]
+    public void NumberValidator_ReturnsFalse_WithInvalidFloatNumbers(int precision, int scale, bool onlyPositive,
+        string number) => TestWithInvalidParameters(precision, scale, onlyPositive, number);
+
+    [TestCase("a")]
+    [TestCase("seven")]
+    [TestCase("one.five")]
+    [TestCase("   ")]
+    [TestCase("")]
+    [TestCase(null)]
+    public void NumberValidator_ReturnsFalse_WithNonNumber(string number)
+        => TestWithInvalidParameters(5, 4, false, number);
+
+    [TestCase("IV")]
+    [TestCase("1 . 1")]
+    [TestCase("1. 1")]
+    [TestCase("1 .1")]
+    [TestCase("10_000")]
+    [TestCase("10 000")]
+    [TestCase("10.")]
+    [TestCase(".1")]
+    [TestCase("+.1")]
+    [TestCase("-.1")]
+    [TestCase("5*3")]
+    public void NumberValidator_ReturnsFalse_WithWrongFormat(string number)
+        => TestWithInvalidParameters(5, 4, false, number);
+
+    [TestCase("1,1")]
+    [TestCase("1.1")]
+    [TestCase("11")]
+    public void NumberValidator_CorrectWork_WithCorrectSeparatorFormat(string number)
+        => TestWithValidParameters(5, 4, false, number);
+
+    [TestCase("+11")]
+    [TestCase("+1111")]
+    [TestCase("+1.111")]
+    [TestCase("-1111")]
+    [TestCase("-1.111")]
+    [TestCase("-1.1")]
+    [TestCase("-11")]
+    [TestCase("+1.1")]
+    [TestCase("1.1")]
+    [TestCase("11")]
+    public void NumberValidator_CorrectWork_WithAndWithoutSign(string number)
+        => TestWithValidParameters(5, 3, false, number);
+
+    private static void TestWithValidParameters(int precision, int scale, bool onlyPositive,
+        string number)
+    {
+        var validator = new NumberValidator(precision, scale, onlyPositive);
+        validator.IsValidNumber(number).Should().BeTrue();
+    }
+
+    private static void TestWithInvalidParameters(int precision, int scale, bool onlyPositive,
+        string number)
+    {
+        var validator = new NumberValidator(precision, scale, onlyPositive);
+        validator.IsValidNumber(number).Should().BeFalse();
     }
 }
