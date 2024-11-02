45cb66adef1bf82defca22b705812f2ac501da9d
Maxim Mineev <mms1401@ya.ru>
2024-11-02 17:04:06 +0500

    ObjectComparison. Рефакторинг второго теста, комментарии кода

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,8 +1,8 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
+
 public class ObjectComparison
 {
     private Person actualTsar;
@@ -14,7 +14,7 @@ public class ObjectComparison
         actualTsar = TsarRegistry.GetCurrentTsar();
         expectedTsar = CreateDefaultTsar();
     }
-    
+
     [Test]
     [Description("Проверка текущего царя")]
     [Category("ToRefactor")]
@@ -25,7 +25,7 @@ public class ObjectComparison
         actualTsar.Height.Should().Be(expectedTsar.Height);
         actualTsar.Weight.Should().Be(expectedTsar.Weight);
 
-        actualTsar.Parent!.Name.Should().Be(expectedTsar.Parent!.Name);
+        actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
         actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
         actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
         actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
@@ -36,24 +36,24 @@ public class ObjectComparison
     public void CheckCurrentTsar_WithCustomEquality()
     {
         // Какие недостатки у такого подхода? 
-        ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
+        // AreEqual не содержит в себе Assert, при падении теста мы не знаем, что произошло (неинформативный вывод)
+        // При расширении класса Person, нам необходимо менять код уже написанного метода AreEqual
+        // и компилятор нам не подскажет, что это нужно сделать
+        // В тесте только вызывается метод AreEqual, приходится писать инфраструктурный код для его создания
+
+        // Плюсы моего подхода:
+        // При расширении класса Person, программа не скомпилируется, если не поменять код (предотвращает ошибки).
+        // Код приходится менять только в генераторах объектов
+        // При расширении класса Person, нам не нужно изменять тест (первый тест придется, компилятор не подскажет)
+        // Логика сравнения перенесена в сам тест, не приходится писать лишний инфраструктурный код
+        // P.S. создание объектов вынесено в Setup. Мы не сравниваем Id, создавать каждый раз новые данные для теста нет необходимости
+        actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
+            options.Excluding(y => y.Id).Excluding(y => y.Parent.Id));
     }
 
-    private bool AreEqual(Person? actual, Person? expected)
-    {
-        if (actual == expected) return true;
-        if (actual == null || expected == null) return false;
-        return
-            actual.Name == expected.Name
-            && actual.Age == expected.Age
-            && actual.Height == expected.Height
-            && actual.Weight == expected.Weight
-            && AreEqual(actual.Parent, expected.Parent);
-    }
-    
     private static Person CreateDefaultTsar()
-        => new ("Ivan IV The Terrible", 54, 170, 70, CreateDefaultParent());
+        => new("Ivan IV The Terrible", 54, 170, 70, CreateDefaultParent());
 
     private static Person CreateDefaultParent()
-        => new ("Vasili III of Russia", 28, 170, 60, null);
+        => new("Vasili III of Russia", 28, 170, 60, null);
 }
