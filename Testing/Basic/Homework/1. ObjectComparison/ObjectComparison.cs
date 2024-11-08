2ced4e17f264d6a34b6965123d829f3586d60b71
Maxim Mineev <mms1401@ya.ru>
2024-11-08 23:23:35 +0500

    ObjectComparison. Вернул `AreEqual`, реализовал `IEquatble` в 'Person

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -15,24 +15,18 @@ public class ObjectComparison
         expectedTsar = CreateDefaultTsar();
     }
 
+    // Плюсы моего подхода:
+    // При расширении класса Person, программа не скомпилируется, если не поменять код (предотвращает ошибки).
+    // Код приходится менять только в генераторах объектов
+    // При расширении класса Person, нам не нужно изменять тест (первый тест придется, компилятор не подскажет)
+    // Засчет рефлексии нам не нужно менять код Equals в Person даже при расширении
+    // Тест в одну строчку
+    // P.S. создание объектов вынесено в Setup. Мы не сравниваем Id, создавать каждый раз новые данные для теста нет необходимости
+    
     [Test]
-    [Description("Проверка текущего царя")]
-    [Category("ToRefactor")]
-    public void CheckCurrentTsar()
-    {
-        actualTsar.Name.Should().Be(expectedTsar.Name);
-        actualTsar.Age.Should().Be(expectedTsar.Age);
-        actualTsar.Height.Should().Be(expectedTsar.Height);
-        actualTsar.Weight.Should().Be(expectedTsar.Weight);
-
-        actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
-        actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
-        actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
-        actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
-    }
+    public void GetCurrentTsar_ShouldReturn_ValidTsar() => actualTsar.Should().BeEquivalentTo(expectedTsar);
 
     [Test]
-    [Description("Альтернативное решение. Какие у него недостатки?")]
     public void CheckCurrentTsar_WithCustomEquality()
     {
         // Какие недостатки у такого подхода? 
@@ -40,15 +34,19 @@ public class ObjectComparison
         // При расширении класса Person, нам необходимо менять код уже написанного метода AreEqual
         // и компилятор нам не подскажет, что это нужно сделать
         // В тесте только вызывается метод AreEqual, приходится писать инфраструктурный код для его создания
+        AreEqual(actualTsar, expectedTsar).Should().BeTrue();
+    }
 
-        // Плюсы моего подхода:
-        // При расширении класса Person, программа не скомпилируется, если не поменять код (предотвращает ошибки).
-        // Код приходится менять только в генераторах объектов
-        // При расширении класса Person, нам не нужно изменять тест (первый тест придется, компилятор не подскажет)
-        // Логика сравнения перенесена в сам тест, не приходится писать лишний инфраструктурный код
-        // P.S. создание объектов вынесено в Setup. Мы не сравниваем Id, создавать каждый раз новые данные для теста нет необходимости
-        actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
-            options.Excluding(y => y.Id).Excluding(y => y.Parent.Id));
+    private static bool AreEqual(Person? actual, Person? expected)
+    {
+        if (actual == expected) return true;
+        if (actual == null || expected == null) return false;
+        return
+            actual.Name == expected.Name
+            && actual.Age == expected.Age
+            && actual.Height == expected.Height
+            && actual.Weight == expected.Weight
+            && AreEqual(actual.Parent, expected.Parent);
     }
 
     private static Person CreateDefaultTsar()
--- Testing/Basic/Homework/1. ObjectComparison/Person.cs
+++ Testing/Basic/Homework/1. ObjectComparison/Person.cs
@@ -1,13 +1,21 @@
-﻿namespace HomeExercise.Tasks.ObjectComparison;
+﻿using System.Reflection;
 
+namespace HomeExercise.Tasks.ObjectComparison;
 
-public class Person
+public class Person : IEquatable<Person>
 {
     public static int IdCounter = 0;
     public int Age, Height, Weight;
     public string Name;
     public Person Parent;
     public int Id;
+    
+    private static readonly FieldInfo[] Fields;
+
+    static Person()
+    {
+        Fields = typeof(Person).GetFields();
+    }
 
     public Person(string name, int age, int height, int weight, Person parent)
     {
@@ -18,4 +26,12 @@ public class Person
         Weight = weight;
         Parent = parent;
     }
+
+    public bool Equals(Person? other)
+        => Fields
+            .Where(field => field.Name != nameof(Id))
+            .Where(field => field.GetValue(this) != null || field.GetValue(other) != null)
+            .All(field => field.GetValue(this) != null && field.GetValue(this)!.Equals(field.GetValue(other)));
+
+    public override bool Equals(object obj) => obj is Person person && Equals(person);
 }
